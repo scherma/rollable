645be0cd1ceab03df1a5aa0d1ce9226d8e4f2753
scherma <github@hexistentialist.com>
2023-08-29 19:50:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://scherma.github.io/rollable/"
+  base: "https://scherma.github.io/rollable/",
   plugins: [
     vue(),
   ],
